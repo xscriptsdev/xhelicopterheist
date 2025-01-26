38c2a892340a455e6bcbbe7c8f2254113b868a08
XSCRIPTS <104461809+xscriptsdev@users.noreply.github.com>
2025-01-26 03:44:40 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,18 +1,17 @@
-ESX = exports["es_extended"]:getSharedObject()
-local framework = Config.Framework == "ESX" and ESX or QBCore
-
 RegisterServerEvent("xhelicopterheist:reward")
 AddEventHandler("xhelicopterheist:reward", function()
     local source = source
 
     if Config.EnableReward then
         if Config.Framework == "ESX" then
+                ESX = exports["es_extended"]:getSharedObject()
             local xPlayer = ESX.GetPlayerFromId(source)
             if xPlayer then
                 xPlayer.addMoney(Config.RewardCash)
                 TriggerClientEvent("ox_lib:notify", source, { title = "Reward", description = string.format(Config.RewardMessage, Config.RewardCash), type = "success" })
             end
         elseif Config.Framework == "QB" then
+                QBCore = exports['qb-core']:GetCoreObject()
             local Player = QBCore.Functions.GetPlayer(source)
             if Player then
                 Player.Functions.AddMoney("cash", Config.RewardCash)
